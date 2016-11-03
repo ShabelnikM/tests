c3bdb796b8cafaa8f4443a83bf323b4153433bcb
Shabelnik Max <maksim.shabelnik@faceit-team.com>
2016-11-03 11:56:56 +0200

    10 fixed

--- SimpleTasks/10.rb
+++ SimpleTasks/10.rb
@@ -2,11 +2,15 @@
 #     которые удовлетворяют двойному неравенству: A[0] < A[i] < A[-1].
 #     Если таких элементов нет, то вывести [ ]
 
-array = [7, 2, 3, 4, 8, 9, 10]
+array = [7, 2, 3, 4, 1, 3, 10]
 
 array.each do |item|
-  if item > array[0] && item < array[-1]
-    p array.find_index(item)
+  case
+  when item > array[0] && item < array[-1]
+    p "Index of A[i]: #{array.find_index(item)}"
     break
+  when item == array.last
+    p []
   end
 end
+
