df8428fa0e3170495c30c7022746d9f5d4900ae1
Shabelnik Max <maksim.shabelnik@faceit-team.com>
2016-11-03 10:01:43 +0200

    1st fixed

--- SimpleTasks/1.rb
+++ SimpleTasks/1.rb
@@ -1,12 +1,9 @@
 # Дан целочисленный массив. Необходимо вывести вначале
 #    его элементы с четными индексами, а затем - с нечетными
 
-array = [ 0, 0, 0, 0, 0, 0, 0]
+array = [ 0, 1, 0, 3, 4, 8, 0]
 
-array.each_with_index { |item, index|  puts "#{item} is even" if index.even? }
-
-array.each do |x|
-  puts "#{x} is odd" if x.odd?
-end
+array.each_with_index { |item, index|  p "#{item} is even" if index.even? }
+array.each_with_index { |item, index|  p "#{item} is odd" if index.odd? }
 
 
