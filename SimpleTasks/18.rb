c1539932276490ae5d18bf4341cc517f66fabe1a
Shabelnik Max <maksim.shabelnik@faceit-team.com>
2016-11-04 11:50:29 +0200

    18 finished

--- SimpleTasks/18.rb
+++ SimpleTasks/18.rb
@@ -7,3 +7,21 @@
 
 array = [{a: 1, b: 4}, {a: 33, b: -8}, {a: -22, b: 23}, {a: -2.01, b: 77}, {a: 15, b: 13}]
 
+positive_array = []
+new_array = array.map { |hash| hash.values }
+new_array.each { |item| item.each { |sub| positive_array << sub if sub > 0 } }
+p positive_array
+
+p array.find { |hash| hash[:a] < 0 }
+
+obj ={
+    a: array.map{|hash| hash[:a]}.inject(0){|sum, item| sum += item},
+    b: array.map{|hash| hash[:b]}.inject(0){|sum, item| sum += item}
+  }
+p obj
+
+p array.sort_by { |hash| hash[:b] }
+
+p array.select { |hash| hash[:b].even? }
+
+p array.any? { |hash| hash[:a] < 0 && hash[:b] < 0 }
